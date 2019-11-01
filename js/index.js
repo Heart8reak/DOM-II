1d046b1306b0d9d632a8d0d857cfd6f7e6fcf2af
Carlos Baez <carlosbaez@Green-Planet.local>
2019-11-01 00:15:45 -0400

    added eventListener mouseover to the logo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+const companyLogo = document.querySelector('.logo-heading');
+
+companyLogo.addEventListener("mouseover", function (event) {
+    event.target.style.color = "green";
+    event.target.style.transform = "skew(180deg, 180deg)";
+    event.target.style.transition = "all 1.5s";
+
+
+    setTimeout(function () {
+        event.target.style.color = "red"
+        event.target.style.transform = "auto";
+    }, 500);
+}, false);
