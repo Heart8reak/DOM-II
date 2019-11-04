e37da4ccbe38cb795fb70920d1d4a73f13db4bd0
Carlos Baez <carlosbaez@Green-Planet.local>
2019-11-03 21:35:40 -0500

    completed dblclick

--- js/index.js
+++ js/index.js
@@ -170,3 +170,44 @@ function reportWindowSize() {
 }
 
 window.onresize = reportWindowSize;
+
+
+// ########################
+// ##################
+// Scroll
+// #################
+// ########################
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function doSomething(scroll_pos) {
+
+}
+
+window.addEventListener('scroll', function (e) {
+    last_known_scroll_position = window.scrollY;
+
+    if (!ticking) {
+        window.requestAnimationFrame(function () {
+            doSomething(last_known_scroll_position);
+            ticking = false;
+        });
+
+        ticking = true;
+    }
+});
+
+
+// ########################
+// ##################
+// dblclick
+// #################
+// ########################
+
+
+const card = document.querySelector('aside');
+
+card.addEventListener('dblclick', function (e) {
+    card.classList.toggle('large');
+})
