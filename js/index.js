9f06ee2a489f215d3b0dfa5c34274fae378c9ffe
Carlos Baez <carlosbaez@Green-Planet.local>
2019-11-03 20:23:18 -0500

    completed focus

--- js/index.js
+++ js/index.js
@@ -135,3 +135,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
     logger.textContent = logger.textContent + `DOMContentLoaded\n;`
 });
 
+
+// ########################
+// ##################
+// Focus
+// #################
+// ########################
+
+
+const password = document.querySelector('input[type="password"]');
+
+password.addEventListener('focus', (event) => {
+    event.target.style.background = 'pink';
+});
+
+password.addEventListener('blur', (event) => {
+    event.target.style.background = '';
+});
+
